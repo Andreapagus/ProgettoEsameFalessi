563d3551316f2f4b5bb4df1afde90bb8053d10ee
Andreapagus <34506169+Andreapagus@users.noreply.github.com>
2022-02-17 12:06:01 +0100

    Update InputOperation.java

--- src/logic/InputOperation.java
+++ src/logic/InputOperation.java
@@ -6,14 +6,15 @@ import java.util.logging.Logger;
 
 public class InputOperation {
 	
+	private InputOperation(){}
+	
 	private static void main(String[] args) {
-		int data = 0;
 		try {
 			byte[] arr = System.in.readAllBytes();
 			//Apro il canale di input dove attendo in ingresso
 			//Uno stream di bytes con cui poter fare operazioni
 			ByteArrayInputStream io = new ByteArrayInputStream(System.in.readAllBytes());
-		
+			int data = 0;
 			for(int i=0;i<arr.length;i++) {
 				//Faccio una semplice somma dell'input
 				data += io.read();
